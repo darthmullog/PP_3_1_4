d266a60dd2389d4257d66a1b3eac21445b9a6456
darthmullog <168669499+darthmullog@users.noreply.github.com>
2024-08-27 22:41:02 +0300

    Delete PP_3_1_4v1.1 directory

--- PP_3_1_4v1.1/src/main/resources/static/deleteUser.js
+++
@@ -1,9 +0,0 @@
-function deleteUser(id) {
-    fetch('http://localhost:8080/admin/delete/' + id, {
-        method: 'DELETE',
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-        .then(response => {
-            $('#' + id).remove();
-        });
-}
--- PP_3_1_4v1.1/src/main/resources/static/listRoles.js
+++
@@ -1,9 +0,0 @@
-function listRoles(user) {
-
-    let roles = "";
-    for (let i = 0; i < user.roles.length; i++) {
-        roles += " " + user.roles[i].name;
-    }
-
-    return roles;
-}
