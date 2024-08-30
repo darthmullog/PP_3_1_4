798528fd602ac0baaf0ea723fe3794d99dc24912
darthmullog <168669499+darthmullog@users.noreply.github.com>
2024-08-30 03:09:16 +0300

    Add files via upload

---
+++ PP_3_1_4v1.1/src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -0,0 +1,19 @@
+package ru.kata.spring.boot_security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class SpringBootSecurityDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "admin";
+        String encodedPassword = encoder.encode(rawPassword);
+        System.out.println(encodedPassword);
+    }
+
+}
